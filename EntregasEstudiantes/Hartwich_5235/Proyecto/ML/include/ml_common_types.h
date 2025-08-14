1f646c7e099d4e94946a30191d0aa6b34f61bd40
Laurent <220237569+LHartw@users.noreply.github.com>
2025-08-14 14:55:03 -0500

    minor

--- EntregasEstudiantes/Hartwich_5235/Proyecto/ML/include/ml_common_types.h
+++
@@ -1,8 +0,0 @@
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-variable" // this flag triggered warnings about code from the Eigen header when run with -march=native
-#include <Eigen/Dense>
-#pragma GCC diagnostic pop
-
-// type definitions used throughout this part of the project
-
-using Index = Eigen::Index;
